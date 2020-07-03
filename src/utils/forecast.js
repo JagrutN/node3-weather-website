8d59c0a760606240c27923b66ee3a198a07c1b95
JagrutN <jagrutjnemade.6p10@gmail.com>
2020-07-03 21:57:32 +0530

    Add about and forecast page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0])
+            callback(undefined, "It is " +body.current.weather_descriptions[0] +". The temperature is " +body.current.temperature)
         }
     })
 }
